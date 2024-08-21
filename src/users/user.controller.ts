cfb237bb08b1f0bf8d173fa04b927e1b1e974da5
adeliamara <adeliamara13@gmail.com>
2024-08-21 03:02:34 -0300

    fix: errors

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -92,7 +92,6 @@ export class UserController {
   }
 
   private errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-    console.error(err.message);
     const statusCode = err.status || 500;
     const message = err.message || 'Internal Server Error';
     res.setHeader('Content-Type', 'application/json');
